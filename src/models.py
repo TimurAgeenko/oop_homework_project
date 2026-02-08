bdad3d4f810ded1fc20e019201004f6ba6d944b3
TimurAgeenko <ageenko.timur.10@gmail.com>
2026-02-09 02:49:15 +0300

    Added new classes

--- src/models.py
+++ src/models.py
@@ -105,3 +105,43 @@ class CategoryIterator:
             return self.category.products_list[self.index]
         else:
             raise StopIteration
+
+
+class Smartphone(Product):
+    """Класс, представляющий смартфон, наследуется от класса Product."""
+
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        price: float,
+        quantity: int,
+        efficiency: float,
+        model: str,
+        memory: int,
+        color: str,
+    ):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+    """Класс, представляющий газонную траву, наследуется от класса Product."""
+
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        price: float,
+        quantity: int,
+        country: str,
+        germination_period: str,
+        color: str
+    ):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
